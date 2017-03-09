4bd58f7ed5ff567187da1412dc2849c28de03fbf
eximus <eximus@localhost.localdomain>
2017-03-09 17:45:54 +0000

    minor fix

--- algorithms/KMP.h
+++ algorithms/KMP.h
@@ -9,20 +9,26 @@ int *compute_pi(char *P, size_t m) {
 
     for (int i = 1; i < m; i++) {
         if (P[i] == P[pi[i - 1] + 1]) {
-            pi[i] = pi[i - 1] + 1
+            pi[i] = pi[i - 1] + 1;
         }
         else {
             P[i] = -1;
         }
     }
+    for (int i = 0; i < m; i++) {
+        printf("%d," , pi[i]);
+    }
+    printf("\n");
     return pi;
 }
 
-void KMP(dynamic_array *T, dynamic_array *P) {
-    size_t n = T->used;
-    size_t m = P->used;
+void KMP(dynamic_array *_T, dynamic_array *_P) {
+    size_t n = _T->used;
+    size_t m = _P->used;
+    char* T = _T->data;
+    char* P = _P->data;
 
-    int *pi = compute_pi(P->data, m);
+    int *pi = compute_pi(P, m);
 
     size_t q = -1;
     for(size_t i = 0; i < n; i++) {
