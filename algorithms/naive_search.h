a0b967117455ad9a1d0c79efd559f68b8d7e26dc
Vasco-jofra <joaovascofranco@hotmail.com>
2017-03-09 17:41:05 +0000

    something

--- algorithms/naive_search.h
+++ algorithms/naive_search.h
@@ -6,10 +6,10 @@
 void naive_online_search(dynamic_array *T, dynamic_array *P) {
     printf("--- Running naive_online_search:\n");
 
-    size_t T_size = T->used;
-    size_t P_size = P->used;
-    for (size_t i = 0; i <= T_size - P_size; i++) {
-        if(strncmp(T->data+i, P->data ,P_size) == 0) {
+    size_t n = T->used;
+    size_t m = P->used;
+    for (size_t i = 0; i <= n - m; i++) {
+        if(strncmp(T->data+i, P->data, m) == 0) {
             // @See: migth print one space too many for mooshack
             printf("%d ", i);
         }
