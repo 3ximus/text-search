6d3a2748b74e8df3bd09dd0098a8b21e6285a6bb
eximus <eximus@localhost.localdomain>
2017-03-09 17:38:50 +0000

    precomputation

--- algorithms/KMP.h
+++ algorithms/KMP.h
@@ -5,9 +5,15 @@
 
 int *compute_pi(char *P, size_t m) {
     int *pi = malloc(m * sizeof(int));
+    pi[0]=-1; /* first element is always zero */
 
-    for(int i = 0; i < m; i++) {
-
+    for (int i = 1; i < m; i++) {
+        if (P[i] == P[pi[i - 1] + 1]) {
+            pi[i] = pi[i - 1] + 1
+        }
+        else {
+            P[i] = -1;
+        }
     }
     return pi;
 }
