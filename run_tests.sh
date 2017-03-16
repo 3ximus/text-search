cbb275ed5c1cd3d3ba59e2f4567930f8731d0bb2
3ximus <fabio_r11@hotmail.com>
2017-03-16 00:03:45 +0000

    better test automation

--- run_tests.sh
+++ run_tests.sh
@@ -13,11 +13,18 @@ make compile-merged
 for in in tests/*.txt; do
 	tst="${in##*/}"
 	out="tests/${tst}.out"
-	echo -e "\t\e[1;38;5;215mRunning $in \e[0m > $out"
+	echo -en "\t\e[1;38;5;215mRunning $in \e[0m > $out"
 	./project < $in > $out
 	# FIX BELLOW
-	echo "Test								Expected"
-	diff -y $out "tests/expected-output/$tst"
+	output=$(diff -y $out "tests/expected-output/$tst")
+	if [[ $output ]]; then
+		echo -e "  [ \e[1;38;5;196mFailed\e[0m ]"
+		echo "Test								Expected"
+		diff -y $out "tests/expected-output/$tst"
+	else
+		echo -e "  [ \e[1;38;5;41Succeded\e[0m ]"
+	fi
+
 done
 
 # clean
